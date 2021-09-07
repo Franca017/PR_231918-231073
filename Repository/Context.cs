d0928dd80d4a9ada911b185230ce326871c533e6
Francisco <franciscoecabanillas@gmail.com>
2021-09-07 16:55:28 -0300

    Context

--- Repository/Context.cs
+++ Repository/Context.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
     public class Context : DbContext
     {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
     }
 }
