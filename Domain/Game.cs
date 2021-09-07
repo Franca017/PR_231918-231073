62f6dce7f1bb41f8349e6a34c891905b4d6f2e97
Francisco <franciscoecabanillas@gmail.com>
2021-09-07 17:16:55 -0300

    Id's Dominio

--- Domain/Game.cs
+++ Domain/Game.cs
@@ -4,6 +4,7 @@ namespace Domain
 {
     public class Game
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
         public int Rating { get; set; }
--- Domain/User.cs
+++ Domain/User.cs
@@ -6,6 +6,7 @@ namespace Domain
 {
     public class User
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public List<Game> PublishedGames { get; set; }
