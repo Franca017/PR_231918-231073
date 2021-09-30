28f4312057a66f921b8a4854ad209050b7617513
Francisco <franciscoecabanillas@gmail.com>
2021-09-30 08:58:34 -0300

    Leve cambio en CommandConstants

--- ProtocolLibrary/CommandConstants.cs
+++ ProtocolLibrary/CommandConstants.cs
@@ -5,19 +5,18 @@ namespace ProtocolLibrary
     public class CommandConstants
     {
         public const int Login =  1;
-        public const int ListGames = 2;
-        public const int Message = 3;
-        public const int Purchase = 4;
-        public const int Publish = 5;
-        public const int Search = 6;
-        public const int FilterRating = 7;
-        public const int ListPublishedGames = 8;
-        public const int ModifyGame = 9;
-        public const int DeleteGame = 10;
-        public const int GetReviews = 11;
-        public const int Rate = 12;
-        public const int DetailGame = 13;
-        public const int Download = 14;
-        public const int ModifyImage = 15;
+        public const int ListGames = Login+1;
+        public const int Purchase = ListGames+1;
+        public const int Publish = Purchase+1;
+        public const int Search = Publish+1;
+        public const int FilterRating = Search+1;
+        public const int ListPublishedGames = FilterRating+1;
+        public const int ModifyGame = ListPublishedGames+1;
+        public const int DeleteGame = ModifyGame+1;
+        public const int GetReviews = DeleteGame+1;
+        public const int Rate = GetReviews+1;
+        public const int DetailGame = Rate+1;
+        public const int Download = DetailGame+1;
+        public const int ModifyImage = Download+1;
     }
 }
