15d86be3997f6b0ad87451fd7ef13310b097c680
Francisco <franciscoecabanillas@gmail.com>
2021-11-22 14:41:17 -0300

    Detalle en servicio del logBuilder (now working)

--- ServiceFactory/ServiceFactory.cs
+++ ServiceFactory/ServiceFactory.cs
@@ -22,7 +22,7 @@ namespace Factory
             _services.AddSingleton<IGamesLogic, GamesLogic>();
             _services.AddSingleton<IUserLogic, UserLogic>();
             _services.AddSingleton<IReviewLogic, ReviewLogic>();
-            _services.AddScoped<ILogBuilderLogic, LogBuilderLogic>();
+            _services.AddSingleton<ILogBuilderLogic, LogBuilderLogic>();
 
             _services.AddSingleton<IGameRepository, GameRepository>();
             _services.AddSingleton<IUserRepository, UserRepository>();
